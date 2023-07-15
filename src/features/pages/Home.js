f59527c368bddc95da74942566f08561db95d1dd
Aminul <aislamrony@gmail.com>
2023-07-15 23:17:15 +0600

    home page design

---
+++ src/features/pages/Home.js
@@ -0,0 +1,15 @@
+import NavBar from "../navbar/Navbar";
+import ProductList from "../product-list/ProductList";
+
+
+function Home() {
+    return (
+        <div>
+            <NavBar>
+                <ProductList></ProductList>
+            </NavBar>
+        </div>
+    );
+}
+
+export default Home;
